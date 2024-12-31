6bb2ca86adf59999581b5e93d75479dfb20d2710
Abhinav <abhinav4848@gmail.com>
2024-12-31 04:36:07 +0000

    completed blade design

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,17 +6,21 @@
 				<meta name="viewport" content="width=device-width, initial-scale=1.0">
 				<meta http-equiv="X-UA-Compatible" content="ie=edge">
 				<title>Pixel Positions</title>
-				@vite(['resources/js/app.js'])
+				<link rel="preconnect" href="https://fonts.googleapis.com">
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+				<link href="https://fonts.googleapis.com/css2?family=DM+Sans&family=Hanken+Grotesk:wght@400;500;600;&display=swap"
+						rel="stylesheet">
+				@vite(['resources/css/app.css', 'resources/js/app.js'])
 		</head>
 
-		<body>
-				<div>
-						<nav>
+		<body class="bg-black text-white font-hanken-grotesk">
+				<div class="px-10">
+						<nav class="flex justify-between items-center py-4 border-b border-white/10">
 								<div>
 										<a href="/"><img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="Company Logo"></a>
 								</div>
 
-								<div>
+								<div class="space-x-6 font-bold">
 										<a href="#">Jobs</a>
 										<a href="#">Careers</a>
 										<a href="#">Salaries</a>
@@ -27,7 +31,7 @@
 										<a href="#">Post a Job</a>
 								</div>
 						</nav>
-						<main>{{ $slot }}</main>
+						<main class="mt-10 max-w-[986px] mx-auto">{{ $slot }}</main>
 				</div>
 		</body>
 
